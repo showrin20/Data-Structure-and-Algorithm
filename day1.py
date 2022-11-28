91f6358a7d83c1d1e96fd7c543660f8e608ac1cf
Showrin <89805548+showrin20@users.noreply.github.com>
2022-11-28 16:12:18 +0600

    This is 100 days of python Bootcamp All Projects

---
+++ day1.py
@@ -0,0 +1,47 @@
+#Write your code below this line 👇
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+a=input("What is your name? ")
+print(len(a))
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c=a
+d=b
+a=d
+b=c
+
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
+#1. Create a greeting for your program.
+print("Welcome")
+#2. Ask the user for the city that they grew up in.
+a=input("Enter City the you grew up in:" )
+#3. Ask the user for the name of a pet.
+b=input("Enter the name of a pet:" )
+#4. Combine the name of their city and pet and show them their band name.
+c=a+b
+#5. Make sure the input cursor shows on a new line:
+print(c)
