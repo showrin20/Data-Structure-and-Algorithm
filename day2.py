0d275ed6a5620fff8e49b1808a3c92a7f5c56110
Showrin <89805548+showrin20@users.noreply.github.com>
2022-11-29 00:22:52 +0600

    This is 100 days of python Bootcamp All Projects

---
+++ day2.py
@@ -0,0 +1,5 @@
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+print(two_digit_number[0],"+" ,two_digit_number[1],"=" ,two_digit_number[0]+two_digit_number[1])
+print(int(two_digit_number[0])+int(two_digit_number[1]))
+
